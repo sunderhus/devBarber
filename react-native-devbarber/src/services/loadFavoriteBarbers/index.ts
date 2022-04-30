6c49f72d16c8842078d55d622bd75102e11ea1a6
Sunderhus <matheus.sunderhus@gmail.com>
2022-04-30 01:58:46 -0300

    feat: create LoadFavoriteBarbers service

---
+++ react-native-devbarber/src/services/loadFavoriteBarbers/index.ts
@@ -0,0 +1,26 @@
+import AsyncStorage from '@react-native-community/async-storage';
+import makeApiUrl from '../apiUrlFactory';
+
+interface Barber {
+  id: number;
+  name: string;
+  avatar: string;
+  stars: number;
+}
+
+export interface LoadFavoriteBarbersResponse {
+  list: Array<Barber>;
+  error: string;
+}
+
+const loadFavoriteBarbers = async (): Promise<LoadFavoriteBarbersResponse> => {
+  const token = await AsyncStorage.getItem('@DevBarber_token');
+  const url = makeApiUrl(`user/favorites?token=${token}`);
+
+  const response = await fetch(url);
+  const parsedResponse = await response.json();
+  console.log(parsedResponse)
+  return parsedResponse as LoadFavoriteBarbersResponse;
+};
+
+export default loadFavoriteBarbers;
